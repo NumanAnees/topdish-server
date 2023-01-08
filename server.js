81e16957d37c6d234ac219df888541fa249b23a9
Numan Anees <67089723+NumanAnees@users.noreply.github.com>
2023-01-09 00:07:58 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -6,19 +6,21 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const path = require("path");
 
-
 const app = express();
 
-//db 
-mongoose.connect(process.env.DATABASE_CLOUD,{useNewUrlParser:true,useUnifiedTopology:true})
-.then(()=>console.log("DB connected"))
-.catch((err)=>console.log(err));
+//db
+mongoose
+  .connect(process.env.DATABASE_CLOUD, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
 //routes
-const authRoutes = require("./routes/auth"); 
-const userRoutes = require('./routes/user');
-const categoryRoutes = require('./routes/category');
-const linkRoutes = require('./routes/link');
-
+const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
+const categoryRoutes = require("./routes/category");
+const linkRoutes = require("./routes/link");
 
 //app-middlewares
 app.use(cors());
@@ -27,11 +29,14 @@ app.use(bodyParser.json());
 // app.use(cors({ origin: process.env.CLIENT_URL }));
 
 //middlewares
-app.use("/api",authRoutes);
-app.use('/api', userRoutes);
-app.use('/api', categoryRoutes);
-app.use('/api', linkRoutes);
+app.use("/api", authRoutes);
+app.use("/api", userRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", linkRoutes);
 
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`API is running on port ${port}`));
